939f94355ce52611ac3449fb9e86cda962ceac4f
carlos112717 <2542@holbertonschool.com>
2022-02-10 15:33:25 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*This Script sends a message*/
+/*Main- serves as the starting point for program execution */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+/*Return- 0 when executed*/
+return (0);
+}
