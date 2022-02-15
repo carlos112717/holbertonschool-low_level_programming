bcf600f393c3a147b75f7af4789ff2377f6995b0
carlos112717 <4542@holbertonschool.com>
2022-02-14 21:47:23 -0500

    _puchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+/* Printout of the putchar function calling it with main from the library */
+#include<main.h>
 /**
+ * The _putchar function will be called with main so that a message is printed
+ *
  * main: Call the functions needed to print a message
  *
  * Return : always return 0
  */
-int main()
+
+int main(void)
 	{
-	  printf("_putchar\n");
+	  _putchar ("_putchar\n");
 	    return (0);
 	}
