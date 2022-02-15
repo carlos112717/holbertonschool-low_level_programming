684da3c6262edc305e43819d65f2d77d3f8b1ba3
carlos112717 <2542@holbertonschool.com>
2022-02-14 19:16:56 -0800

    Print Alphabet with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @author Carlos A. Cortes
+ * @brief Algorithm to print the alphabet in lowercase.
+ * @version 0.1
+ * @date 2022-02-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <main.h>
+
+/**
+ * main function
+ *
+ * Returns: 0 if the program ends successfully
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; c++)
+
+	_putchar("%c", alphabet);
+	-putchar("\n");
+	return (0);
+
+}
